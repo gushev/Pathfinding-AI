75393a8cb967f7414e4da04369d56f1ad0f9e7d8
Georgi Ushev <ushev.georgi@gmail.com>
2018-05-25 09:57:07 +0300

    include a bigger default board

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	    Board board = new Board(5, 5, 3);
-	    //Board board = new Board(12, 18, 60);
+	    //Board board = new Board(5, 5, 3);
+	    Board board = new Board(12, 18, 60);
 	    Game game = new Game(board);
         game.play();
     }
